98e4a8013f8496cb0aee60de2ef106ee7afb01a4
Maitreyee Mhasakar <maitreyee.mhasakar@gmail.com>
2020-11-11 12:35:57 -0600

    Add files via upload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/Maitreyee1/gatsby-simplefolio'}
+                  href={url || 'https://github.com/Maitreyee1'}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
@@ -43,6 +43,13 @@ const Footer = () => {
             Maitreyee Mhasakar
           </a>
         </p>
+        <p className="footer__text">
+          © {new Date().getFullYear()} - Template developed by{' '}
+          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
+            Jacobo Martínez
+          </a>
+        </p>
+
 
         {isEnabled && <GithubButtons />}
       </Container>
